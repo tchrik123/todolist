b3ef6a427cff89956c5478ad24e72362cbe06497
Nikoloz Tchrikishvili <124171511+tchrik123@users.noreply.github.com>
2023-04-15 16:52:24 +0400

    Add files via upload

---
+++ to do list/app.js
@@ -0,0 +1,6 @@
+const input = document.querySelector(".input");
+const btn = document.querySelector(".addBtn");
+const done = document.querySelector(".done");
+const close = document.querySelector(".close");
+
+console.log(input, btn, done, close);
